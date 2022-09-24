5d10a4e91ed8e8b4bc414bf508901d1c80469735
Sara_11 <92929722+sarawalid99@users.noreply.github.com>
2022-09-24 22:43:52 +0200

    Create txteditor.h

---
+++ txteditor.h
@@ -0,0 +1,25 @@
+#ifndef FUNCTIONS-HEADER_H_INCLUDED
+
+#define FUNCTIONS-HEADER_H_INCLUDED
+#include "functions-header.cpp"
+using namespace std;
+void appending();
+void dsply_cntnt();
+void clear_file();
+void encrypt_file();
+void decrypt_file();
+void open_file(string name1);
+void merging();
+void countlines();
+void countwords();
+void countchar();
+void turn_to_lower();
+void turn_to_upper();
+void cap_each_1st_alpha();
+void search_for_word();
+void search_num_of_word();
+void save();
+void exit(string name);
+
+
+#endif // FUNCTIONS-HEADER_H_INCLUDED
